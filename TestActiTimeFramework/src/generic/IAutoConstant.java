334b99ba0bd065e78633c579244217712a467299
dipal <dipal@LAPTOP-4A3HGMQ4>
2022-06-16 10:20:23 +0530

    Dipali

---
+++ TestActiTimeFramework/src/generic/IAutoConstant.java
@@ -0,0 +1,10 @@
+package generic;
+
+public class IAutoConstant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TestActiTimeFramework/src/generic/WebDriverCommonLib.java
@@ -0,0 +1,10 @@
+package generic;
+
+public class WebDriverCommonLib {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
